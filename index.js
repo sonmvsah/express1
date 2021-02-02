b8730b9d46bc70acd9a21eff6f67597ef6cd8a71
Yon <yon@Yon.local>
2021-02-03 00:25:23 +0700

    Update 2021/2/03 00:25 AM

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var express = require('express')
 var app = express()
 const port = 3005;
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true }))
 
+var users = [{ id: 1, name: "mai viet son" },
+    { id: 2, name: "Nguyen thi hai yen" }
+];
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -11,8 +16,38 @@ app.get('/', (req, res) => {
     res.render('home', { title: "mai vietson", message: "a thi la ma" });
 
 });
+app.get('/users', (req, res) => {
+    res.render('users/index', {
+        users: users
+    });
+});
 
+// app.get('/view/:userId', (req, res) => {
+//     res.render('users/view', obj);
 
+// });
+app.get('/users/search', function(req, res) {
+    var keyword = req.query.q.toLowerCase();
+    var matchUser = users.filter(function(x) {
+        return x.name.toLowerCase().indexOf(keyword) != -1
+    });
+    res.render('users/index', {
+        users: matchUser
+    });
+
+});
+
+app.get('/users/create', function(req, res) {
+    res.render('users/create');
+});
+app.post('/users/create', function(req, res) {
+    var name = req.body.name;
+    users.push({ name: name });
+    res.render('users/index', {
+        users: users
+    });
+    console.log(users);
+});
 
 
 
